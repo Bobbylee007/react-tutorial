f9aaf27fa8122710c24bebc9a6f2459809c1b774
Bobbylee007 <bobbyleeaustine@gmail.com>
2026-02-16 10:52:10 +0100

    create a react component and JSX

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// JSX Rules
+// return a single element
+// div/ section / article or fragment
+// use camelCase for html attribute
+// return a single element
+// formatting
+
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Greeting />);
---
+++ src/topics/01-component-JSX.js
@@ -0,0 +1,28 @@
+// stateless functional component
+// always return JSX
+
+// function Greeting() {
+//   return <h1>hello world</h1>
+// }
+
+// Nested JSX
+// function Greeting() {
+//   return (
+//     <div>
+//       <h1>hello world</h1>
+//     </div>
+//   );
+// }
+
+// const Greeting = () => {
+//   return React.createElement('h1', {}, 'hellow world')
+// }
+
+// Nested JSX
+// const Greeting = () => {
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h1", {}, "hello world"),
+//   );
+// };
