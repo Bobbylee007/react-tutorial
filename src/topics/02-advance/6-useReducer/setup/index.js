54da5b1256456ecbc5ad6730f4e052f1d38748dd
Bobbylee007 <bobbyleeaustine@gmail.com>
2026-03-18 09:40:01 +0100

    useReducer completion

--- src/topics/02-advance/6-useReducer/setup/index.js
+++ src/topics/02-advance/6-useReducer/setup/index.js
@@ -1,49 +1,44 @@
 import React, { useState, useReducer } from "react";
 import Modal from "./modal";
 import { data } from "../../../../data";
+
 //reducer function
+import { reducer } from "./reducer";
 
-const reducer = (state, action) => {
-  console.log(state);
-  if (action.type === "ADD_ITEM") {
-    const newPeople = [...state.people, action.paypload];
-    return {
-      ...state,
-      people: newPeople,
-      isModalOpen: true,
-      modalContent: "item added",
-    };
-  }
-
-  if (action.type === "NO_VALUE") {
-    return { ...state, isModalOpen: true, modalContent: "please enter value" };
-  }
-
-  throw new Error("no mathing action type");
-};
-
+//state func pointer to defaultState
 const defaultState = {
   people: [],
   isModalOpen: false,
   modalContent: "",
 };
+
+// component
 const Index = () => {
-  const [name, setName] = useState("");
+  const [name, setName] = useState('');
   const [state, dispatch] = useReducer(reducer, defaultState);
 
+  //form control
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
-      const newPeople = { id: new Date().getTime().toString(), name };
-      dispatch({ type: "ADD_ITEM", paypload: newPeople });
-      setName();
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "ADD_ITEM", payload: newItem });
+      setName('');
     } else {
       dispatch({ type: "NO_VALUE" });
     }
   };
+  const closeModal = () => {
+    dispatch({ type: "CLOSE_MODAL" });
+  };
+
+  // UI
   return (
     <>
-      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
+      {state.isModalOpen && (
+        <Modal closeModal={closeModal} modalContent={state.modalContent} />
+      )}
+
       <form onSubmit={handleSubmit} className="form">
         <div>
           <input
@@ -56,8 +51,16 @@ const Index = () => {
       </form>
       {state.people.map((person) => {
         return (
-          <div key={person.id}>
+          <div key={person.id} className="item">
             <h4>{person.name}</h4>
+            <button
+              className="btn"
+              onClick={() =>
+                dispatch({ type: "REMOVE_ITEM", payload: person.id })
+              }
+            >
+              remove
+            </button>
           </div>
         );
       })}
